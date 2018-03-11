7c94f4df790ff4fb426c30a2d639f0944edde22e
Sinan Topaloglu <topaloglusinan@sabanciuniv.edu>
2018-03-11 10:50:25 +0300

    Small fix for routing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,12 @@ class Api::UsersController < ApplicationController
 		render json: @users, status: :ok
 	end
 
+	def show
+		@user = User.find(params[:id])
+
+		render json: @user, status: :ok
+	end
+
 	def create
 		@user = User.new(user_params)
 
