27932c82e7e2a5aa25524174a1a2ccf6522188ca
Sinan Topaloglu <topaloglusinan@sabanciuniv.edu>
2018-03-19 13:51:52 +0300

    ActiveRecord Associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+	belongs_to :customer
+	has_many :products, through: :delivery_lists
+	has_one :invoice
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+	has_many :categories, through: :product_categories
+	has_many :orders, through: :delivery_lists
 end
